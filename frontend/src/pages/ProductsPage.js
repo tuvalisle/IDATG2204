13ad684990b48e4efcc274b492c2e9162c737806
Tuva <tuvalis@stud.ntnu.no>
2025-04-01 09:34:31 +0200

    Frontend

---
+++ frontend/src/pages/ProductsPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function ProductsPage() {
+  return (
+    <div>
+      <h1>Products</h1>
+      <p>Here you will find all our products.</p>
+    </div>
+  );
+}
+
+export default ProductsPage;
+
