38a07aaa5f13116ae1e3d1092e9aa7e734a647e6
Aurora Olsen <aurorlo@stud.ntnu.no>
2025-04-07 09:51:56 +0200

    enkel styling på produkter, forstatt mye som må endres

--- electromart-frontend/src/App.js
+++ electromart-frontend/src/App.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './styles.css';
 import './AuthPage.css';
+import './products.css';
 import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
 import AuthPage from './pages/AuthPage';
